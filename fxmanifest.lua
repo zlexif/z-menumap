0d5c60dfb5dc38490cf08856c685d27ec141d485
zlexif <146517988+zlexif@users.noreply.github.com>
2024-02-09 23:53:23 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+author 'ZDev'
+description 'A script for handling map animations when the pause menu is active.'
+version '1.0.0'
+
+client_scripts {
+    'map.lua' 
+}
