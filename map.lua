43e7ea5757b0c15f88e8d9e5f751effa5eab2305
zlexif <146517988+zlexif@users.noreply.github.com>
2024-02-10 09:26:31 +0200

    added globabl variable to ensure prop deletion

--- map.lua
+++ map.lua
@@ -1,8 +1,6 @@
-
 local isPauseMenuActive = false
-local mapProp = "prop_tourist_map_01" 
-local mapBone = 60309 
-
+local mapProp = "prop_tourist_map_01"
+local mapPropObject = nil 
 function startMapAnimation()
     local playerPed = PlayerPedId()
 
@@ -16,32 +14,25 @@ function startMapAnimation()
         Wait(1)
     end
 
-    local mapObject = CreateObject(GetHashKey(mapProp), 0, 0, 0, true, true, true)
-    local rightHandIndex = GetPedBoneIndex(playerPed, 57005) -- Right hand bone index
-
-    local positionOffset = {x = 0.09, y = 0.02, z = -0.08}
-    local rotation = {x = -80.0, y = 0.0, z = 0.0}
-
-    AttachEntityToEntity(mapObject, playerPed, rightHandIndex, positionOffset.x, positionOffset.y, positionOffset.z, rotation.x, rotation.y, rotation.z, true, true, false, true, 1, true)
+    mapPropObject = CreateObject(GetHashKey(mapProp), 0, 0, 0, true, true, true) -- Store the prop object globally
+    local rightHandIndex = GetPedBoneIndex(playerPed, 57005)
+    AttachEntityToEntity(mapPropObject, playerPed, rightHandIndex, 0.09, 0.02, -0.08, -80.0, 0.0, 0.0, true, true, false, true, 1, true)
     
     TaskPlayAnim(playerPed, "amb@world_human_tourist_map@male@base", "base", 8.0, -8.0, -1, 49, 0, false, false, false)
 end
 
-
 function stopMapAnimation()
     ClearPedTasks(PlayerPedId())
     RemoveAnimDict("amb@world_human_tourist_map@male@base")
-    local playerPed = PlayerPedId()
-    local object = GetClosestObjectOfType(GetEntityCoords(playerPed), 5.0, GetHashKey(mapProp), false, false, false)
-    if DoesEntityExist(object) then
-        DeleteObject(object)
+    if mapPropObject and DoesEntityExist(mapPropObject) then
+        DeleteObject(mapPropObject)
+        mapPropObject = nil -- Clear the reference after deletion
     end
 end
 
--- Main loop to check for pause menu status
 Citizen.CreateThread(function()
     while true do
-        Citizen.Wait(300) -- Check every 300ms to reduce performance impact
+        Citizen.Wait(300)
 
         if IsPauseMenuActive() and not isPauseMenuActive then
             isPauseMenuActive = true
